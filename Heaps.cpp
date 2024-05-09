43851c09c5db0675a6878c78bacb8112927af5a9
Madankh <khadkasuman0000@gmail.com>
2024-05-09 15:54:43 +0545

    main

--- Heaps.cpp
+++ Heaps.cpp
@@ -31,6 +31,34 @@ class heap{
             cout<<arr[i]<<" ";
         }cout<<endl;
     }
+
+    void deletefromHeap(){
+        if(size == 0){
+            cout<<"Nothings to delete";
+            return;
+        }
+
+        arr[1] = arr[size];
+        size--;
+
+        // take root node to it;s corrent position
+        int i=1;
+        while(i<size){
+            int leftIndex = 2*i;
+            int rightIndex = 2*i+1;
+
+            if(leftIndex < size && arr[i] < arr[leftIndex]){
+                swap(arr[i], arr[leftIndex]);
+                i = leftIndex;
+            }
+            else if(rightIndex < size && arr[i] < arr[rightIndex]){
+                swap(arr[i], arr[rightIndex]);
+                i = rightIndex;
+            }else{
+                return;
+            }
+        }
+    }
 };
 
 int main(){
@@ -40,6 +68,9 @@ int main(){
     h.insert(53);
     h.insert(52);
     h.insert(54);
+    h.print();
+
+    h.deletefromHeap();
     h.print();
     return 0;
 }
