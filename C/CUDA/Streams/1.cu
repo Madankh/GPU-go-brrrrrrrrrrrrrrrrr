d04c13c8dc4b54d8fb0081a0c4f34a5d379be147
Madankh <khadkasuman0000@gmail.com>
2024-10-28 11:05:05 +0545

    update

--- C/CUDA/Streams/1.cu
+++ C/CUDA/Streams/1.cu
@@ -1,14 +1,14 @@
 #include<cuda_runtime.h>
 #include<stdio.h>
-
 #define CHECK_CUDA_ERROR(val) check((val), #val, __FILE__, __LINE__)
 
-template <typename>
-
-template<typename T>
-void check(T err, const char* const func, const char* const file, const int line){
-    if(err != cudaSuccess){
-        fprintf(stderr, "CUDA error at %s:%d code=%d(%s)\%s\" \n", file, line, static_cast<unsigned int>(err), cudaGetErrorString(err), func);
+template<typename T> 
+void check(T err, const char* const func, const char* const file, const int line) {
+    if(err != cudaSuccess) {
+        // Fixed the escape sequence in the error message
+        fprintf(stderr, "CUDA error at %s:%d code=%d(%s) \"%s\"\n", 
+                file, line, static_cast<unsigned int>(err), 
+                cudaGetErrorString(err), func);
         exit(EXIT_FAILURE);
     }
 }
@@ -47,6 +47,11 @@ int main(){
     CHECK_CUDA_ERROR(cudaStreamCreate(&stream1));
     CHECK_CUDA_ERROR(cudaStreamCreate(&stream2));
 
+
+    // Copy result back to host asynchronously
+    CHECK_CUDA_ERROR(cudaMemcpyAsync(d_A,h_A, size, cudaMemcpyHostToDevice, stream1));
+    CHECK_CUDA_ERROR(cudaMemcpyAsync(d_B,h_B, size, cudaMemcpyHostToDevice, stream2));
+
     //lAUNCHG KERNELS
     int threadsperBlock = 256;
     int blocksPerGrid = (numElements + threadsperBlock - 1);
@@ -56,6 +61,28 @@ int main(){
     CHECK_CUDA_ERROR(cudaMemcpyAsync(h_C, d_C, size, cudaMemcpyDeviceToHost, stream1));
 
     // Synchronize streams
-    CHECK_CUDA_ERROR()
+    CHECK_CUDA_ERROR(cudaStreamSynchronize(stream1));
+    CHECK_CUDA_ERROR(cudaStreamSynchronize(stream2));
+
+    // Verify result
+    for (int i = 0; i < numElements; ++i) {
+        if (fabs(h_A[i] + h_B[i] - h_C[i]) > 1e-5) {
+            fprintf(stderr, "Result verification failed at element %d!\n", i);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("Test PASSED\n");
+
+    // Clean up
+    CHECK_CUDA_ERROR(cudaFree(d_A));
+    CHECK_CUDA_ERROR(cudaFree(d_B));
+    CHECK_CUDA_ERROR(cudaFree(d_C));
+    CHECK_CUDA_ERROR(cudaStreamDestroy(stream1));
+    CHECK_CUDA_ERROR(cudaStreamDestroy(stream2));
+    free(h_A);
+    free(h_B);
+    free(h_C);
+
     return 0;
 }
