3ca6bde91408eff92eecd1680f0d6413bee70cef
Madankh <khadkasuman0000@gmail.com>
2024-10-31 08:06:30 +0545

    new

---
+++ C/CUDA/CUDA_API/CUBLAS/01_Hgemm_Sgemm.cu
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<cuda_runtime.h>
+#include<cublas_v2.h>
+#include<cuda_fp16.h>
+
+#define M 3
+#define K 4
+#define N 2
+
+#define CHECK_CUDA(call) { \
+    cudaError_t err = call; \
+    if (err != cudaSuccess) { \
+        fprintf(stderr, "CUDA error in %s:%d: %s\n", __FILE__, __LINE__, cudaGetErrorString(err)); \
+        exit(EXIT_FAILURE); \
+    } \
+} 
+
+#define CHECK_CUBLAS(call) { \
+    cublasStatus_t status = call; \
+    if (status != CUBLAS_STATUS_SUCCESS) { \
+        fprintf(stderr, "cuBLAS error in %s:%d: %d\n", __FILE__, __LINE__, status); \
+        exit(EXIT_FAILURE); \
+    } \
+}
+
+#undef PRINT_MATRIX
+#define PRINT_MATRIX(mat, rows, cols) \
+    for (int i = 0; i < rows; i++) { \
+        for (int j = 0; j < cols; j++) \
+            printf("%8.3f ", mat[i * cols + j]); \
+        printf("\n"); \
+    } \
+    printf("\n");
+
+void cpu_matmul(float *A, float *B, float *C){
+    for
+}
